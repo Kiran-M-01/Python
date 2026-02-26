7cba9a1814706120f3eb498cb442bd136a8f0980
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-02-26 11:06:11 +0530

    simple inheritance

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class got :
+    def char1(self):
+        print("Jon is best charecter")
+class hotd(got):
+    def char2(self):
+        print("deamon is best charecter")
+obj = hotd()
+
+obj.char2()
+obj.char1()
+    
