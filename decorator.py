a127438ccc366bfba21666b6dfccc9988339b957
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-11 15:32:22 +0530

    decorator

--- decorator.py
+++ decorator.py
@@ -1,23 +1,44 @@
-def instagram(func):
-    def wrapper(*args, **kwargs):
-        print("open instagram")
-        print("login")
-        func(*args, **kwargs)
-        print("logout from instagram")
+# def instagram(func):
+#     def wrapper(*args, **kwargs):
+#         print("open instagram")
+#         print("login")
+#         func(*args, **kwargs)
+#         print("logout from instagram")
+#     return wrapper
+
+# @instagram
+# def sangu():
+#     print("chat with risha")
+
+# @instagram
+# def harsha():
+#     print("chat with pooja")
+
+# @instagram
+# def darshan():
+#     print("chat with raghu")
+
+# sangu()
+# harsha()
+# darshan()
+
+
+
+
+# 2
+import time 
+def delay_time(func):
+    def wrapper(*args,**kwargs):
+        time.sleep(10)
+        r = func(*args,**kwargs)
+        print(r)
     return wrapper
 
-@instagram
-def sangu():
-    print("chat with risha")
+@delay_time
+def reverse(s):
+    rev = ''
+    for i in s :
+        rev = i + rev
+    return rev
 
-@instagram
-def harsha():
-    print("chat with pooja")
-
-@instagram
-def darshan():
-    print("chat with raghu")
-
-sangu()
-harsha()
-darshan()
+reverse("hello")
