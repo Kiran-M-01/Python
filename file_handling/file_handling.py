237d3635439c4b01e086eb1e332cc3d41db573f7
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-12 15:10:54 +0530

    file handling

---
+++ file_handling/file_handling.py
@@ -0,0 +1,11 @@
+# FILE HANDLING
+# WRITE 
+# f = open("sam.txt", 'w')
+# f.write('hello everyone')
+# f.close()
+
+# f = open("sam.txt", 'w')
+# f.writelines('hello everyone\n how are you\n')
+# f.close()
+
+# READ
