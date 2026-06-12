5a95932b8246e7c15b4d433cc3e98b69677f1116
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-12 15:20:42 +0530

    file handling

--- file_handling/file_handling.py
+++ file_handling/file_handling.py
@@ -1,14 +1,15 @@
 # FILE HANDLING
-# WRITE 
+
+# -------WRITE ----------- 
 # f = open("sam.txt", 'w')
 # f.write('hello everyone')
 # f.close()
 
 # f = open("sam.txt", 'w')
-# f.writelines('good morning everyone \nhow are you \nI am fine')
+# f.writelines('good morning everyone \nhow are you \nI am fine\n')
 # f.close()
 
-# READ
+# --------READ----
 # f = open("sam.txt", 'r')
 # data = f.read()      # COMPLETE FILE
 # print(data)
@@ -19,7 +20,13 @@
 # print(data)
 # f.close()
 
-f = open("sam.txt", 'r')
-data = f.readline()  # FIRST LINE
-print(data)
+# f = open("sam.txt", 'r')
+# data = f.readlines()  # IN THE FORM OF LIST
+# print(data)
+# f.close()
+
+# ----APPEND----
+f = open("sam.txt", 'a')
+f.write('today is 12th jun')
 f.close()
+
