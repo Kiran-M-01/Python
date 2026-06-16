cd11547698261029a14f7425eef64a4d27b86626
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-16 14:37:03 +0530

    csv file handling

--- file_handling_CSV.py
+++ file_handling_CSV.py
@@ -1,12 +1,12 @@
 import csv
 # CREATING / WRITING CSV FILE
-f = open("file.csv",'w')
-a = csv.writer(f)
-a.writerow(['name',"age","ph_no",'marks'])
-a.writerows([['darshan',"22","998877",'100'],['harsha',"21","998866",'99'],['sang',"23","998855",'98'],])
-f.close()
+# f = open("file.csv",'w')
+# a = csv.writer(f)
+# a.writerow(['name',"age","ph_no",'marks'])
+# a.writerows([['darshan',"22","998877",'100'],['harsha',"21","998866",'99'],['sang',"23","998855",'98'],])
+# f.close()
 
 # READING CSV FILE
-# f = open("file.csv",'r')
-# data = csv.reader(f)
-# print(data)
+f = open("file.csv",'r')
+data = csv.reader(f)
+print(list(data))
