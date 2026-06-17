d5aacaa4808799ba81c06c5ecb4479da81a96275
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-17 15:19:11 +0530

    exception handling

--- exception_handling.py
+++ exception_handling.py
@@ -2,12 +2,19 @@ try:
     a = int(input("enter an number : "))
     b = int(input("enter an number : "))
     print(a / c)
-except ValueError :
-    print("enter only integer")
-except ZeroDivisionError :
-    print("can not be dedvided by Zero")
-except NameError :
-    print("variable not declared")
+# except ValueError as e:
+#     # print("enter only integer")
+#     print(e)
+# except ZeroDivisionError as e :
+#     # print("can not be dedvided by Zero")
+#     print(e)
+# except NameError as e:
+#     # print("variable not declared")
+#     print(e)
+
+
+except Exception as e:         # TO HANDLE ALL ERRORS
+    print(e)
 else:
     print("no errors found")
 finally:
