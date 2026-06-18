789b16d46a6a310f5d034e35a624058a1654e501
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-18 14:59:36 +0530

    raising an user defined  error

--- raise_error.py
+++ raise_error.py
@@ -27,10 +27,22 @@
 #     raise NegativeNumberError("you entered negative number")
 
 # 2
-class NotEligibleError(Exception):
+# class NotEligibleError(Exception):
+#     pass
+# n = int(input("enter a number :"))
+# if n >= 18:
+#     print("Eligible")
+# else:
+#     raise NotEligibleError("you are not eligible")
+
+# 3
+class NotSpecialCharecter(Exception):
     pass
-n = int(input("enter a number :"))
-if n >= 18:
-    print("Eligible")
+ch = input("enter a char :")
+if (32 <= ord(ch) <= 47) or \
+   (58 <= ord(ch) <= 64) or \
+   (91 <= ord(ch) <= 96) or \
+   (123 <= ord(ch) <= 126):
+    print("Special Character")
 else:
-    raise NotEligibleError("you are not eligible")
+    raise NotSpecialCharecter("not a special charecter")
