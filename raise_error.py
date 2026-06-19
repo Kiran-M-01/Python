ef008271dad7fd72090c61adf77ce3b18a8f505f
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-19 14:15:03 +0530

    rasing error

--- raise_error.py
+++ raise_error.py
@@ -20,6 +20,7 @@
 # 1
 # class NegativeNumberError(Exception):
 #     pass
+
 # n = int(input("enter a number :"))
 # if n > 0:
 #     print(n)
@@ -29,6 +30,7 @@
 # 2
 # class NotEligibleError(Exception):
 #     pass
+
 # n = int(input("enter a number :"))
 # if n >= 18:
 #     print("Eligible")
@@ -36,13 +38,33 @@
 #     raise NotEligibleError("you are not eligible")
 
 # 3
-class NotSpecialCharecter(Exception):
+# class NotSpecialCharecter(Exception):
+#     pass
+
+# ch = input("enter a char :")
+# if (32 <= ord(ch) <= 47) or \
+#    (58 <= ord(ch) <= 64) or \
+#    (91 <= ord(ch) <= 96) or \
+#    (123 <= ord(ch) <= 126):
+#     print("Special Character")
+# else:
+#     raise NotSpecialCharecter("not a special charecter")
+
+# 4
+class InsufficientBalanceError(Exception):
     pass
-ch = input("enter a char :")
-if (32 <= ord(ch) <= 47) or \
-   (58 <= ord(ch) <= 64) or \
-   (91 <= ord(ch) <= 96) or \
-   (123 <= ord(ch) <= 126):
-    print("Special Character")
-else:
-    raise NotSpecialCharecter("not a special charecter")
+
+
+class account():
+    def __init__(self,bal):
+        self.bal = bal
+    def withdraw(self):
+        amt = int(input("enter amount to withdraw : "))
+        if self.bal > amt :
+            self.bal -= amt
+            print("withdraw success full",self.bal)
+        else:
+            raise InsufficientBalanceError("enter less amount")
+
+ob = account(1000)
+ob.withdraw()
