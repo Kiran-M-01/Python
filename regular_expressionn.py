d5b108b05ad50424e197b515752bd6351f1a706a
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-29 14:02:05 +0530

    regular expression

--- regular_expressionn.py
+++ regular_expressionn.py
@@ -1,9 +1,9 @@
 import re
-s = 'sangamesh is working in google his dob id 13-6-2004 and his contact number is 9876543219 and email is sang@gmail.com'
+s = 'sangamesh is working in google his dob id 13-6-2004 and his contact number is 9876543219 and email is sang123@gmail.com'
 
 # pat = r'[0-9]{1,2}\-[0-9]{1,2}\-[0-9]{4}'
 
 # pat = r'[6-9][0-9]{9}'
-pat = r'\d*\@\w*\.?\w*'
+pat = r'\w*\@\w*\.?\w*'
 data = re.findall(pat,s)
 print(data)
