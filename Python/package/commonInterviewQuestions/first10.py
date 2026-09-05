e843016a4c7e7b437aa9d7e65955c52efefe3656
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-09-05 14:33:14 +0530

    factorial and fibonacci

--- Python/package/commonInterviewQuestions/first10.py
+++ Python/package/commonInterviewQuestions/first10.py
@@ -29,24 +29,48 @@
 # print(reverse(12345564321))
 
 
-# 5) Prime number
-def isPrime(n):
-    if n < 2:
-        return False
+# 5), 5) Prime number  , range of prime numbers
+# def isPrime(n):
+#     if n < 2:
+#         return False
     
-    for i in range(2,n):
-        if n % i == 0:
-            # return f'not prime'
-            return False
+#     for i in range(2,n):
+#         if n % i == 0:
+#             # return f'not prime'
+#             return False
 
-    # return f'Prime number'
-    return True
+#     # return f'Prime number'
+#     return True
 
-# print(isPrime(3))
+# # print(isPrime(3))
 
-def PrimeRange(n):
-    for num in range(1,n+1):
-        if isPrime(num):
-            print(num)
+# def PrimeRange(m,n):
+#     for num in range(m,n+1):
+#         if isPrime(num):
+#             print(num)
 
-PrimeRange(20)
+# PrimeRange(10, 20)
+
+
+# 7)Find Factorial of a Number
+# def fact(n):
+#     res = 1
+#     for i in range(1,n+1):
+#         res = res*i
+#     return res
+
+# print(fact(7))
+
+# 8. Generate Fibonacci Series
+def fib(n):
+    a = 0
+    b = 1
+    for i in range(n+1):
+        print(a)
+        a,b = b,a+b
+
+print(fib(7))
+
+
+    
+
